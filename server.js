eb78525d5621bfc88176ce6fb6406cc8de1e76cf
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2023-06-17 17:10:12 +0600

    error solved for mongodb

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require("express")
 const app = express()
 const port = 3001;
@@ -9,8 +13,18 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const indexRouter = require('./routes/index')
+// mongoDB connection 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true // Add this line for the new MongoDB driver
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
+// importing main index route
+const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || port, () => {
